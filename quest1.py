cb100dafaf54aa484d462c7f0256ac56ac004653
olsch88 <olsch88@googlemail.com>
2025-11-05 21:54:18 +0100

    quest1, part3

--- quest1.py
+++ quest1.py
@@ -30,15 +30,29 @@ def solve_part2(names: list[str], instructions: list[str]) -> str:
             step_total+=step
         else:
             step_total-=step
-    print(step_total)
     return names[position+step_total]
+
+def solve_part3(names: list[str], instructions: list[str]) -> str:
+    length = len(names)
+    for instr in instructions:
+        direction = instr[0]
+        step = int(instr[1:])
+        
+        if direction=="R":
+            names[0], names[step%length]=names[step%length], names[0]
+        else:
+            names[0], names[-step%length]=names[-step%length], names[0]    
+        
+    return names[0]
 
 def tests():
     assert solve_part1(*read_names_and_instructions("q1_p1_example.txt"))=="Fyrryn"
     assert solve_part2(*read_names_and_instructions("q1_p2_example.txt"))=="Elarzris"
+    assert solve_part3(*read_names_and_instructions("q1_p3_example.txt"))=="Drakzyph"
     
 if __name__ == "__main__":
     tests()
     
     print(solve_part1(*read_names_and_instructions("q1_p1_input.txt")))
     print(solve_part2(*read_names_and_instructions("q1_p2_input.txt")))
+    print(solve_part3(*read_names_and_instructions("q1_p3_input.txt")))
