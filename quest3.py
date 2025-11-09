b4a36f7c5b2d1347b8be273fee3ed6ffb756d8f8
olsch88 <olsch88@googlemail.com>
2025-11-09 20:05:33 +0100

    added timings

--- quest3.py
+++ quest3.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import time
 
 def read_crates(filename: str)-> list[int]:
     with open(filename) as f:
@@ -30,7 +31,12 @@ def tests():
 
 if __name__=="__main__":
     tests()
-    print(read_crates("q3_p1_example.txt"))
-    print(solve_part1(read_crates("q3_p1_input.txt")))
-    print(solve_part2(read_crates("q3_p2_input.txt")))
-    print(solve_part3(read_crates("q3_p3_input.txt")))
+    start=time.perf_counter()    
+    print(f"Solution Part1: {solve_part1(read_crates("q3_p1_input.txt"))}")
+    print(f"Time Part 1: {time.perf_counter()-start:.3f} s")
+    start=time.perf_counter()
+    print(f"Solution Part2: {solve_part2(read_crates("q3_p2_input.txt"))}")
+    print(f"Time Part 2: {time.perf_counter()-start:.3f} s")
+    start=time.perf_counter()
+    print(f"Solution Part3: {solve_part3(read_crates("q3_p3_input.txt"))}")
+    print(f"Time Part 3: {time.perf_counter()-start:.3f} s")
