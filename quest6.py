ed9da1ee3cafd2bfefb47809fafa542c463b7216
olsch88 <olsch88@googlemail.com>
2025-11-13 20:24:02 +0100

    quest 6, part3

--- quest6.py
+++ quest6.py
@@ -29,15 +29,32 @@ def solve_part2(notes: str) -> int:
     return total_pairs
 
 
-def solve_part3(notes: str) -> int:
-    return 0
+def find_pairs(notes:str, repeats: int, distance: int)->int:
+    pairs=0
+    length = len(notes)
+    full_notes= notes*repeats
+    for pos, char in enumerate(full_notes):
+        #search for novice
+        if char != char.lower():
+            continue
+        start =max(0,pos-distance )
+        stop = min(pos+distance+1,len(full_notes))
+        pairs+=full_notes[start:stop].count(char.upper())
+    return pairs
+        
+            
+
+def solve_part3(notes: str, rep=1, distance=10) -> int:
+    number = find_pairs(notes, rep, distance)
+    return number
 
 
 def tests():
     assert solve_part1(read_notes("q6_p1_example1.txt")) == 5
     assert solve_part2(read_notes("q6_p2_example.txt")) == 11
-
-
+    assert solve_part3(read_notes("q6_p3_example1.txt"))==34
+    assert solve_part3(read_notes("q6_p3_example2.txt"), 2,10)==72
+    assert solve_part3(read_notes("q6_p3_example2.txt"), 1000,1000)==3442321
 if __name__ == "__main__":
     tests()
     start = time.perf_counter()
@@ -46,6 +63,6 @@ if __name__ == "__main__":
     start = time.perf_counter()
     print(f"Solution Part2: {solve_part2(read_notes("q6_p2_input.txt"))}")
     print(f"Time Part 2: {time.perf_counter()-start:.3f} s")
-    # start = time.perf_counter()
-    # print(f"Solution Part3: {solve_part3(*read_data_part2("q5_p3_input.txt"))}")
-    # print(f"Time Part 3: {time.perf_counter()-start:.3f} s")
+    start = time.perf_counter()
+    print(f"Solution Part3: {solve_part3(read_notes("q6_p3_input.txt"), 1000, 1000)}")
+    print(f"Time Part 3: {time.perf_counter()-start:.3f} s")
