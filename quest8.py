1dd90e9efd1d3f29a7f983a17e3b3273bd19f5ef
olsch88 <olsch88@googlemail.com>
2025-11-15 21:30:10 +0100

    formating

--- quest8.py
+++ quest8.py
@@ -1,61 +1,64 @@
 import time
 
-def read_data(filename:str)-> list[int]:
+
+def read_data(filename: str) -> list[int]:
     with open(filename) as f:
         data = f.readline()
-    numbers = [int(n)for n in data.split(",")]
+    numbers = [int(n) for n in data.split(",")]
     return numbers
 
-def intersect(line1:tuple[int, int], line2: tuple[int, int])->bool:
+
+def intersect(line1: tuple[int, int], line2: tuple[int, int]) -> bool:
     """linien schneiden sich, wenn
     ein punkt kleiner ist als das maximum und größer als das minimum un der andere punkt größer ist als das maximum des anderen punktes
     """
-    if max(line2)>max(line1) and min(line2)<max(line1) and min (line2)> min(line1):
+    if max(line2) > max(line1) and min(line2) < max(line1) and min(line2) > min(line1):
         return True
-    if max(line1)>max(line2) and min(line1)<max(line2) and min (line1)> min(line2):
+    if max(line1) > max(line2) and min(line1) < max(line2) and min(line1) > min(line2):
         return True
     return False
 
-def solve_part1(numbers:list[int], circle_length:int)->int:
-    length=len(numbers)
-    crossings=0
+
+def solve_part1(numbers: list[int], circle_length: int) -> int:
+    length = len(numbers)
+    crossings = 0
     for i, n in enumerate(numbers[:-1]):
-        if abs(n-numbers[i+1])==circle_length//2:
-            crossings+=1
+        if abs(n - numbers[i + 1]) == circle_length // 2:
+            crossings += 1
     return crossings
 
-def solve_part2(numbers:list[int])->int:
-    pairs=[]
-    for i,number in enumerate(numbers[:-1]):
-        pairs.append((number, numbers[i+1]))
-    
-    intersects=0
+
+def solve_part2(numbers: list[int]) -> int:
+    pairs = []
+    for i, number in enumerate(numbers[:-1]):
+        pairs.append((number, numbers[i + 1]))
+
+    intersects = 0
     for i, pair in enumerate(pairs[1:]):
         for j in range(i):
-            intersects+= intersect(pair, pairs[j])
-    
+            intersects += intersect(pair, pairs[j])
+
     return intersects
 
 
-
-
 def tests():
-    assert solve_part1(read_data("q8_p1_example.txt"),8)==4
-    assert intersect((1,5),(2,5))==False
-    assert intersect((2,5),(1,5))==False
-    assert intersect((2,6),(1,5))==True
-    assert intersect((1,5),(2,6))==True
-    assert intersect((8,6),(1,5))==False
-    assert intersect((6,8),(1,5))==False
-    assert intersect((7,3),(1,5))==True
-    assert intersect((7,3),(8,6)) ==True
-    assert intersect((7,3),(1,4)) ==True
-    assert intersect((7,8),(8,6)) == False
-    assert intersect((7,5),(1,5)) == False
-    assert intersect((7,5),(6,2)) == True
-    assert solve_part2(read_data("q8_p2_example.txt"))==21
+    assert solve_part1(read_data("q8_p1_example.txt"), 8) == 4
+    assert intersect((1, 5), (2, 5)) == False
+    assert intersect((2, 5), (1, 5)) == False
+    assert intersect((2, 6), (1, 5)) == True
+    assert intersect((1, 5), (2, 6)) == True
+    assert intersect((8, 6), (1, 5)) == False
+    assert intersect((6, 8), (1, 5)) == False
+    assert intersect((7, 3), (1, 5)) == True
+    assert intersect((7, 3), (8, 6)) == True
+    assert intersect((7, 3), (1, 4)) == True
+    assert intersect((7, 8), (8, 6)) == False
+    assert intersect((7, 5), (1, 5)) == False
+    assert intersect((7, 5), (6, 2)) == True
+    assert solve_part2(read_data("q8_p2_example.txt")) == 21
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     tests()
     start = time.perf_counter()
     print(f"Solution Part1: {solve_part1(read_data("q8_p1_input.txt"),32)}")
