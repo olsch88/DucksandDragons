bc9b185d9131c5709090a5ac7630049a00bee116
olsch88 <olsch88@googlemail.com>
2025-11-17 21:12:21 +0100

    formating

--- quest9.py
+++ quest9.py
@@ -2,6 +2,7 @@ import time
 from itertools import permutations
 from collections import deque
 
+
 def read_data(filename: str) -> list[tuple[int, str]]:
     with open(filename) as f:
         data = f.readlines()
@@ -53,13 +54,14 @@ def solve_part2(gene_sequences: list[tuple[int, str]]) -> int:
             sum_similarities += calc_degree_similarity(per)
     return sum_similarities // 2  # dividing by two, because we find every family twice
 
-def get_family(related: dict[int, set[int]], start:int)->set[int]:
-    found=set()
-    current=start
+
+def get_family(related: dict[int, set[int]], start: int) -> set[int]:
+    found = set()
+    current = start
     queue = deque()
     found.add(start)
     queue.append(current)
-    while len(queue)!=0:
+    while len(queue) != 0:
         current = queue.popleft()
         for element in related[current]:
             if element not in found:
@@ -70,36 +72,36 @@ def get_family(related: dict[int, set[int]], start:int)->set[int]:
 
 def solve_part3(gene_sequences: list[tuple[int, str]]) -> int:
     perms = permutations(gene_sequences, 3)
-    related:dict[int, set[int]]= dict()
-    for i in range(1, len(gene_sequences)+1):
-        related[i]=set((i,))
+    related: dict[int, set[int]] = dict()
+    for i in range(1, len(gene_sequences) + 1):
+        related[i] = set((i,))
     for per in perms:
         if is_child(per[0][1], per[1][1], per[2][1]):
-            related[per[0][0]].add( per[1][0])
-            related[per[0][0]].add( per[2][0])
-            
-            related[per[1][0]].add( per[0][0])
-            related[per[1][0]].add( per[2][0])
-            
-            related[per[2][0]].add( per[0][0])
-            related[per[2][0]].add( per[1][0])
-            
-    len_largest=0
-    largest=set()
-    checked=set()
-    for i in range(1, len(gene_sequences)+1):
+            related[per[0][0]].add(per[1][0])
+            related[per[0][0]].add(per[2][0])
+
+            related[per[1][0]].add(per[0][0])
+            related[per[1][0]].add(per[2][0])
+
+            related[per[2][0]].add(per[0][0])
+            related[per[2][0]].add(per[1][0])
+
+    len_largest = 0
+    largest = set()
+    checked = set()
+    for i in range(1, len(gene_sequences) + 1):
         if i in checked:
             continue
         checked.add(i)
 
-        current_family =get_family(related, i)
+        current_family = get_family(related, i)
 
-        current_family_length=len(current_family)
+        current_family_length = len(current_family)
         checked.update(current_family)
-        if current_family_length> len_largest:
-            len_largest =current_family_length
-            largest=current_family
-    
+        if current_family_length > len_largest:
+            len_largest = current_family_length
+            largest = current_family
+
     return sum(largest)
 
 
