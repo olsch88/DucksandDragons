15061e2e7ebfd626c863bcd4eccf6ea23731e13e
olsch88 <olsch88@googlemail.com>
2025-11-22 22:46:32 +0100

    quest 13 complete

--- quest13.py
+++ quest13.py
@@ -6,6 +6,13 @@ def read_data(filename: str)-> list[int]:
     dials =[int(line.strip()) for line in data]
     return dials
 
+def read_data_part2(filename:str)->list[str]:
+    with open(filename) as f:
+        data = f.readlines()
+    ranges=[line.strip() for line in data]
+    return ranges
+
+
 def construct_dial(numbers: list[int])-> list[int]:
     dials=[0]*(len(numbers)+1)
     dials[0]=1
@@ -16,24 +23,44 @@ def construct_dial(numbers: list[int])-> list[int]:
             dials[-i//2]=num
     return dials
 
+def construct_dial_part2(numbers:list[str])-> list[int]:
+    clockwise=[]
+    counterclockwise=[]
+    for i, span in enumerate(numbers):
+        start=int(span.split("-")[0])
+        stop=int(span.split("-")[1])
+        if i%2==0:
+            clockwise+=list(range(start, stop+1))
+        else: 
+            counterclockwise+=list(range(start, stop+1))
+    return [1]+clockwise+counterclockwise[::-1]
+
+
 def solve_part1(dials:list[int])-> int:
     dials=construct_dial(dials)
     return dials[(2025%len(dials))]
 
+def solve_part2(numbers:list[str])-> int:
+    dials =construct_dial_part2(numbers)
+    return dials[(20252025%len(dials))]
+
+def solve_part3(numbers:list[str])-> int:
+    dials =construct_dial_part2(numbers)
+    return dials[(202520252025%len(dials))]
+
 def tests():
     assert solve_part1(read_data("q13_p1_example.txt"))==67
-    # assert solve_part2(read_data("q12_p2_example.txt"))==58
+    assert solve_part2(read_data_part2("q13_p2_example.txt"))==30 
     # assert solve_part3(read_data("q12_p3_example1.txt"))==14
 
 if __name__ == "__main__":
     tests()
     start = time.perf_counter()
-    print(construct_dial(read_data("q13_p1_example.txt")))
     print(f"Solution Part1: {solve_part1(read_data("q13_p1_input.txt"))}")
     print(f"Time Part 1: {time.perf_counter()-start:.3f} s")
-    # start = time.perf_counter()
-    # print(f"Solution Part2: {solve_part2(read_data("q12_p2_input.txt"))}")
-    # print(f"Time Part 2: {time.perf_counter()-start:.3f} s")
-    # start = time.perf_counter()
-    # print(f"Solution Part3: {solve_part2(read_data("q11_p3_input.txt"))}")
-    # print(f"Time Part 3: {time.perf_counter()-start:.3f} s")
+    start = time.perf_counter()
+    print(f"Solution Part2: {solve_part2(read_data_part2("q13_p2_input.txt"))}")
+    print(f"Time Part 2: {time.perf_counter()-start:.3f} s")
+    start = time.perf_counter()
+    print(f"Solution Part3: {solve_part3(read_data_part2("q13_p3_input.txt"))}")
+    print(f"Time Part 3: {time.perf_counter()-start:.3f} s")
